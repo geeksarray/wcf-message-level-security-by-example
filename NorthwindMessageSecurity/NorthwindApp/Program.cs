8318740cd115482984b8f8156d30c4bfa390aedb
geeksarray <36873556+geeksarray@users.noreply.github.com>
2020-08-31 19:38:46 +0530

    intital files

---
+++ NorthwindMessageSecurity/NorthwindApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NorthwindApp.ProductServiceRef; 
+
+namespace NorthwindApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductsClient client = new ProductsClient();
+            string cateName = client.GetCategoryName(1);
+            Console.WriteLine(cateName);
+            Console.Read();
+        }
+    }
+}
